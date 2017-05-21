9dd2afd56f01c0c81735d5538477ff7c2521de14
Adel Mandanas <mandanas.adel@gmail.com>
2017-05-21 17:34:17 +0800

    Fix the interpreter code such that it now allows if statements to be multi-line

--- netflux.py
+++ netflux.py
@@ -10,9 +10,17 @@ except IndexError:
     print("You forgot to input the file containing your code!")
     print("Type:\tpython3 run.py <filename.txt>")
 
+temp = ""
 for line in f:
     try:
-        s = line.rstrip()
+        if "end" in line:
+            temp += line.rstrip()
+            parser.parse(temp)
+            temp = ""
+        elif "if" in line or len(temp) > 0:
+            temp += line.rstrip()
+            continue
+        else:
+            parser.parse(line.rstrip())
     except EOFError:
         break
-    parser.parse(s)
